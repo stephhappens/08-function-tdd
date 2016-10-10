c060cd6d9ca629234c7ffa3882a630ac3699f887
stephhappens <stephanie.n.provence@gmail.com>
2016-10-10 18:24:46 -0500

    starting assignment

--- app/blackjack/select-action.js
+++ app/blackjack/select-action.js
@@ -2,5 +2,11 @@ import shouldSplit from './should-split';
 import shouldHit from './should-hit';
 
 export default function (card1, card2) {
+  if shouldHit(cardValue(card1, card2)) {
+    return 'hit';
+  }
 
+  if shouldSplit(card1, card2) {
+    return 'split';
+  }
 }
