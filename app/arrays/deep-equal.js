eb6c037d542d9045f64ffd572753aee858df4cd9
stephhappens <stephanie.n.provence@gmail.com>
2016-10-11 18:30:41 -0500

    solve first three errors

--- app/arrays/deep-equal.js
+++ app/arrays/deep-equal.js
@@ -1,3 +1,19 @@
 export default function deepEquals(a, b) {
+  if (a.length !== b.length) {
+    return false;
+  }
 
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] !== b[i]) {
+      return false;
+    }
+  }
+
+  for (let s = 0; s < b.length; s++) {
+    if (b[s] !== a[s]) {
+      return false;
+    }
+
+    return true;
+  }
 }
