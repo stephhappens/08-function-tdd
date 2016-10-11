34d23e11e81a287786f7643c4c44fa71ac658e91
Stephanie Winn <Stephanie@Lappy-Pro.local>
2016-10-11 15:41:11 -0500

    resolve final error

--- app/blackjack/select-action.js
+++ app/blackjack/select-action.js
@@ -1,12 +1,12 @@
 import shouldSplit from './should-split';
 import shouldHit from './should-hit';
 
-export default function (card1, card2) {
-  if shouldHit(cardValue(card1, card2)) {
+export default function (card1, card2)  {
+  if (shouldHit(card1, card2) === true) {
     return 'hit';
-  }
-
-  if shouldSplit(card1, card2) {
+  } else if (shouldSplit(card1, card2) === true) {
     return 'split';
+  } else {
+    return 'stay';
   }
 }
